0f24b2952df2ce4408f245c939445e941b384250
Giancarlo Faranda <giancarlo.faranda@gmail.com>
2022-05-22 22:48:11 -0400

    Batch pipeline improvements (#64)
    - Prevent blocking execution when using `DataflowRunner`
    
    - Remove default argument for `--subscription` pipeline argument
    
    - Add examples for `--input` and `--subscription` pipeline arguments
    
    - Syntax fixes for `run_pipeline_batch.sh`

--- modules/import_har.py
+++ modules/import_har.py
@@ -4,6 +4,7 @@ import logging
 import apache_beam as beam
 from apache_beam.io.gcp.bigquery import BigQueryWriteFn
 from apache_beam.options.pipeline_options import PipelineOptions, StandardOptions
+from apache_beam.runners import DataflowRunner
 
 from modules import constants, utils
 from modules.transformation import ImportHarJson, ReadHarFiles, WriteBigQuery
@@ -11,12 +12,15 @@ from modules.transformation import ImportHarJson, ReadHarFiles, WriteBigQuery
 
 def parse_args(argv):
     parser = argparse.ArgumentParser()
-    parser.add_argument("--input", dest="input", help="Input file to process.")
+    parser.add_argument(
+        "--input",
+        dest="input",
+        help="Input file to process. Example: gs://httparchive/crawls/*Jan_1_2022"
+    )
     parser.add_argument(
         "--subscription",
         dest="subscription",
-        default="projects/httparchive/subscriptions/har-gcs-pipeline",
-        help="Pub/Sub subscription",
+        help="Pub/Sub subscription. Example: `projects/httparchive/subscriptions/har-gcs-pipeline`",
     )
     return parser.parse_known_args(argv)
 
@@ -35,50 +39,55 @@ def run(argv=None):
 
     # TODO add metric counters for files in, processed, written to GCP & BQ
 
-    with beam.Pipeline(options=pipeline_options) as p:
-        parsed = (
-            p
-            | ReadHarFiles(known_args.subscription, known_args.input)
-            | "ParseHar" >> beam.ParDo(ImportHarJson()).with_outputs("page", "requests")
-        )
-        pages, requests = parsed
-        requests = requests | "FlattenRequests" >> beam.FlatMap(
-            lambda elements: elements
-        )
+    p = beam.Pipeline(options=pipeline_options)
 
-        pages_errors = pages | "WritePagesToBigQuery" >> WriteBigQuery(
-            table=lambda row: utils.format_table_name(row, "pages"),
-            schema=constants.bigquery["schemas"]["pages"],
-            streaming=standard_options.streaming,
-        )
+    parsed = (
+        p
+        | ReadHarFiles(known_args.subscription, known_args.input)
+        | "ParseHar" >> beam.ParDo(ImportHarJson()).with_outputs("page", "requests")
+    )
+    pages, requests = parsed
+    requests = requests | "FlattenRequests" >> beam.FlatMap(
+        lambda elements: elements
+    )
 
-        requests_errors = requests | "WriteRequestsToBigQuery" >> WriteBigQuery(
-            table=lambda row: utils.format_table_name(row, "requests"),
-            schema=constants.bigquery["schemas"]["requests"],
-            streaming=standard_options.streaming,
-        )
+    pages_errors = pages | "WritePagesToBigQuery" >> WriteBigQuery(
+        table=lambda row: utils.format_table_name(row, "pages"),
+        schema=constants.bigquery["schemas"]["pages"],
+        streaming=standard_options.streaming,
+    )
 
-        # deadletter logging
-        if standard_options.streaming:
-            (
-                pages_errors[BigQueryWriteFn.FAILED_ROWS]
-                | "PrintPagesErrors"
-                >> beam.FlatMap(
-                    lambda e: logging.error(f"Could not load page to BigQuery: {e}")
-                )
+    requests_errors = requests | "WriteRequestsToBigQuery" >> WriteBigQuery(
+        table=lambda row: utils.format_table_name(row, "requests"),
+        schema=constants.bigquery["schemas"]["requests"],
+        streaming=standard_options.streaming,
+    )
+
+    # deadletter logging
+    if standard_options.streaming:
+        (
+            pages_errors[BigQueryWriteFn.FAILED_ROWS]
+            | "PrintPagesErrors"
+            >> beam.FlatMap(
+                lambda e: logging.error(f"Could not load page to BigQuery: {e}")
             )
-            (
-                requests_errors[BigQueryWriteFn.FAILED_ROWS]
-                | "PrintRequestsErrors"
-                >> beam.FlatMap(
-                    lambda e: logging.error(f"Could not load request to BigQuery: {e}")
-                )
+        )
+        (
+            requests_errors[BigQueryWriteFn.FAILED_ROWS]
+            | "PrintRequestsErrors"
+            >> beam.FlatMap(
+                lambda e: logging.error(f"Could not load request to BigQuery: {e}")
             )
+        )
 
-        # TODO implement deadletter for FILE_LOADS?
-        #  FAILED_ROWS not implemented for BigQueryBatchFileLoads in this version of beam (only _StreamToBigQuery)
-        # pages_result[BigQueryBatchFileLoads.DESTINATION_JOBID_PAIRS] | beam.Map(lambda e: print(f"jobidpair: {e}"))
-        # pages_result[BigQueryBatchFileLoads.DESTINATION_FILE_PAIRS] | beam.Map(lambda e: print(f"files: {e}"))
-        # pages_result[BigQueryBatchFileLoads.DESTINATION_COPY_JOBID_PAIRS] | beam.Map(lambda e: print(f"copies: {e}"))
+    # TODO implement deadletter for FILE_LOADS?
+    #  FAILED_ROWS not implemented for BigQueryBatchFileLoads in this version of beam (only _StreamToBigQuery)
+    # pages_result[BigQueryBatchFileLoads.DESTINATION_JOBID_PAIRS] | beam.Map(lambda e: print(f"jobidpair: {e}"))
+    # pages_result[BigQueryBatchFileLoads.DESTINATION_FILE_PAIRS] | beam.Map(lambda e: print(f"files: {e}"))
+    # pages_result[BigQueryBatchFileLoads.DESTINATION_COPY_JOBID_PAIRS] | beam.Map(lambda e: print(f"copies: {e}"))
 
-        # TODO detect DONE file, move temp table to final destination, shutdown pipeline (if streaming)
+    # TODO detect DONE file, move temp table to final destination, shutdown pipeline (if streaming)
+
+    pipeline_result = p.run()
+    if not isinstance(p.runner, DataflowRunner):
+        pipeline_result.wait_until_finish()
--- run_pipeline_batch.sh
+++ run_pipeline_batch.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 # shellcheck disable=SC1143,SC2211,SC2215
 python3 run_pipeline.py \
-  --input=gs://httparchive/crawls/*Jan_1_2022 \
+  --input=gs://httparchive/crawls/*May_12_2022 \
   --runner=DataflowRunner \
   --project=httparchive \
-#  --temp_location=gs://httparchive-staging/dataflow/temp \
-#  --staging_location=gs://httparchive-staging/dataflow/staging \
   --temp_location=gs://httparchive-staging/experimental/temp \
   --staging_location=gs://httparchive-staging/experimental/staging \
   --region=us-west1 \
-  --setup_file=./setup.py
+  --setup_file=./setup.py \
   --machine_type=n1-standard-32 \
   --worker_disk_type=compute.googleapis.com/projects//zones//diskTypes/pd-ssd
