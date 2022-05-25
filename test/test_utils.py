c6bd21faa844dfb0ceeacca658017b57d4eaed99
Giancarlo Faranda <giancarlo.faranda@gmail.com>
2022-05-25 09:50:02 -0400

    `test_clamp_integers()` more DRY

--- test/test_utils.py
+++ test/test_utils.py
@@ -141,7 +141,7 @@ class Test(TestCase):
     def test_clamp_integers(self):
         b = utils.BIGQUERY_MAX_INT + 10
         cols = ['a', 'b', 'c', 'd']
-        data = {'a': 1, 'b': utils.BIGQUERY_MAX_INT + 10, 'c': None}
+        data = {'a': 1, 'b': b, 'c': None}
         with self.assertLogs(level="WARNING") as log:
             utils.clamp_integers(data, cols)
             self.assertEqual(data['a'], 1)
