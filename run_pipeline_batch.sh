3392568d71d850aa8d7a1f59520190d6286d899d
Rick Viscomi <rviscomi@users.noreply.github.com>
2022-05-26 01:41:03 +0000

    env

--- run_pipeline_batch.sh
+++ run_pipeline_batch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # shellcheck disable=SC1143,SC2211,SC2215
 python3 run_pipeline.py \
-  --input=gs://httparchive/crawls/*May_12_2022 \
+  --input=gs://httparchive/crawls/test-May_25_2022 \
   --runner=DataflowRunner \
   --project=httparchive \
   --temp_location=gs://httparchive-staging/experimental/temp \
@@ -9,4 +9,5 @@ python3 run_pipeline.py \
   --region=us-west1 \
   --setup_file=./setup.py \
   --machine_type=n1-standard-32 \
-  --worker_disk_type=compute.googleapis.com/projects//zones//diskTypes/pd-ssd
+  --worker_disk_type=compute.googleapis.com/projects//zones//diskTypes/pd-ssd \
+  --noauth_local_webserver
