585fb9a0d90f6c1a7f27ac4354dd2829f97647d2
Giancarlo Faranda <giancarlo.faranda@gmail.com>
2022-06-25 18:28:47 -0400

    Partitioning: add unit test

--- test/test_non_summary_pipeline.py
+++ test/test_non_summary_pipeline.py
@@ -1,9 +1,17 @@
-from unittest import TestCase
+from unittest import TestCase, mock
 
 from modules.non_summary_pipeline import partition_step
 
 
 class TestNonSummaryPipeline(TestCase):
+
+    @mock.patch("modules.non_summary_pipeline.get_page_url", lambda _: "example.com")
+    def test_partition_step_home_page(self):
+        har = {"log": {"pages": [{"_metadata": {"crawl_depth": 0}}]}, "date": None, "client": None}
+        partitions = 4
+        min_partition = 1
+        self.assertTrue(min_partition <= partition_step(har, partitions) <= partitions)
+
     def test_partition_step_secondary_page(self):
         # test for failure case / zero
         har = {"log": {"pages": [{"_metadata": {"crawl_depth": 1}}]}}
