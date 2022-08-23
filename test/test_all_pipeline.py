015f090fadaea6206bfec7682b54599dc21a3c85
Giancarlo Faranda <giancarlo.faranda@gmail.com>
2022-08-23 00:15:00 -0400

    Improve test coverage (#134)

--- test/test_all_pipeline.py
+++ test/test_all_pipeline.py
@@ -7,10 +7,12 @@ from modules import import_all
 
 class TestAllPipeline(TestCase):
     def test_create_pipeline_serialization(self):
-        # batch/GCS file glob
-        p = import_all.create_pipeline(["--input", "foo"])
-        beam.Pipeline.from_runner_api(p.to_runner_api(), p.runner, p._options)
+        tests = [
+            ("input", ["--input", "gs://project/bucket"]),
+            ("input_file", ["--input_file", "gs://project/bucket/manifest.txt"]),
+        ]
 
-        # batch/GCS file listing
-        p = import_all.create_pipeline(["--input_file", "bar"])
-        beam.Pipeline.from_runner_api(p.to_runner_api(), p.runner, p._options)
+        for name, input in tests:
+            with self.subTest(name, input=input):
+                p = import_all.create_pipeline(input)
+                beam.Pipeline.from_runner_api(p.to_runner_api(), p.runner, p._options)
