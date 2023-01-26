16f40fc63d3d93d1b55d47322ccc6e2601892d2e
Giancarlo Faranda <giancarlo.faranda@gmail.com>
2023-01-26 05:07:30 +0000

    testing

--- build_flex_template.sh
+++ build_flex_template.sh
@@ -5,9 +5,18 @@ set -u
 
 BUILD_TAG=$(date +"%Y-%m-%d_%H-%M-%S")
 
-for type in all combined
-do
-    gcloud builds submit --substitutions=_TYPE="${type}",_BUILD_TAG="${BUILD_TAG}" .
-done
+# for type in all combined
+# do
+#     gcloud builds submit --substitutions=_TYPE="${type}",_BUILD_TAG="${BUILD_TAG}" .
+# done
+
+>&2 echo log error spew1
+echo log spew
+echo log spew
+echo log spew
+>&2 echo log error spew2
+echo log spew
+echo log spew
+echo log spew
 
 echo "${BUILD_TAG}"
