018f959435ec1f5e5650df55983905dcdebdfd73
Barry Pollard <barrypollard@google.com>
2023-07-21 11:35:53 +0100

    Add sleep

--- build_flex_template.sh
+++ build_flex_template.sh
@@ -7,6 +7,10 @@ BUILD_TAG=$(date -u +"%Y-%m-%d_%H-%M-%S")
 
 gcloud builds submit --substitutions=_TYPE="all",_BUILD_TAG="${BUILD_TAG}" .
 
+# Sleep to ensure the previous command completes as in GitHub actions it
+# tries to run the builds in parallel which fails.
+sleep 600
+
 gcloud builds submit --substitutions=_TYPE="combined",_BUILD_TAG="${BUILD_TAG}" .
 
 echo "${BUILD_TAG}"
