31934c228015e0000c9dfe2b885a07a9475ffcb4
Barry Pollard <barrypollard@google.com>
2023-07-21 11:15:51 +0100

    Remove FOR loop from build script

--- build_flex_template.sh
+++ build_flex_template.sh
@@ -5,9 +5,8 @@ set -u
 
 BUILD_TAG=$(date -u +"%Y-%m-%d_%H-%M-%S")
 
-for type in all combined
-do
-    gcloud builds submit --substitutions=_TYPE="${type}",_BUILD_TAG="${BUILD_TAG}" .
-done
+gcloud builds submit --substitutions=_TYPE="all",_BUILD_TAG="${BUILD_TAG}" .
+
+gcloud builds submit --substitutions=_TYPE="combined",_BUILD_TAG="${BUILD_TAG}" .
 
 echo "${BUILD_TAG}"
